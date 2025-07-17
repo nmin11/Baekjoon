8b0d4107b4a256a70bdd43072ce6e372d761e624
Min Namgung <nmin1124@gmail.com>
2025-07-17 12:16:25 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 1.15 ms, Memory: 86.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
@@ -0,0 +1,51 @@
+class Solution {
+    int len, now, opStart, opEnd;
+    
+    private int toSeconds(String time) {
+        String[] parts = time.split(":");
+        int m = Integer.parseInt(parts[0]);
+        int s = Integer.parseInt(parts[1]);
+        
+        return m * 60 + s;
+    }
+    
+    private String toTime(int seconds) {
+        int m = seconds / 60;
+        int s = seconds % 60;
+        
+        return String.format("%02d:%02d", m, s);
+    }
+    
+    private void skipOp() {
+        if (this.opStart <= this.now && this.now < this.opEnd) {
+            this.now = this.opEnd;
+        }
+    }
+    
+    private void runCommand(String command) {
+        switch(command) {
+            case "prev":
+                this.now = (this.now - 10 < 0) ? 0 : this.now - 10;
+                break;
+            case "next":
+                this.now = (this.len < this.now + 10) ? this.len : this.now + 10;
+                break;
+            default: return;
+        }
+    }
+    
+    public String solution(String video_len, String pos, String op_start, String op_end, String[] commands) {
+        this.len = toSeconds(video_len);
+        this.now = toSeconds(pos);
+        this.opStart = toSeconds(op_start);
+        this.opEnd = toSeconds(op_end);
+        
+        skipOp();
+        for (int i = 0; i < commands.length; i++) {
+            runCommand(commands[i]);
+            skipOp();
+        }
+        
+        return toTime(this.now);
+    }
+}
