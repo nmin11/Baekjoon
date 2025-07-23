0b3d56352d76247e14db1a56822cb80119d84ae1
Min Namgung <nmin1124@gmail.com>
2025-07-23 12:57:30 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.06 ms, Memory: 86.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
@@ -0,0 +1,29 @@
+class Solution {
+    private int castingTime, healPerSec, bonusHeal, maxHp;
+    
+    public int solution(int[] bandage, int health, int[][] attacks) {
+        this.castingTime = bandage[0];
+        this.healPerSec = bandage[1];
+        this.bonusHeal = bandage[2];
+        this.maxHp = health;
+        
+        int curTime = 0;
+        int maxTime = attacks[attacks.length - 1][0];
+        
+        for (int i = 0; i < attacks.length; i++) {
+            int healDuration = attacks[i][0] - curTime - 1;
+            if (healDuration > 0) {
+                int healAmount = healPerSec * healDuration;
+                int bonusHealCount = healDuration / castingTime;
+                healAmount += bonusHeal * bonusHealCount;
+                health = Math.min(health + healAmount, maxHp);
+            }
+            
+            health -= attacks[i][1];
+            if (health <= 0) return -1;
+            curTime = attacks[i][0];
+        }
+        
+        return health;
+    }
+}
