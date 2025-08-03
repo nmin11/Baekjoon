d7811fd8a24d40e95ee1d48f33983f87035f4dc8
Min Namgung <nmin1124@gmail.com>
2025-08-03 12:33:34 +0900

    [level 1] Title: 과일 장수, Time: 80.77 ms, Memory: 141 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -1,25 +1,12 @@
-import java.util.*;
+import java.util.Arrays;
 
 class Solution {
     public int solution(int k, int m, int[] score) {
-        score = Arrays.stream(score)
-            .boxed()
-            .sorted(Comparator.reverseOrder())
-            .mapToInt(Integer::intValue)
-            .toArray();
-        
-        int pointer = 0;
         int answer = 0;
+        Arrays.sort(score);
         
-        while(pointer + m <= score.length) {
-            for (int i = pointer; i < pointer + m; i++) {
-                if (score[i] < k) {
-                    k = score[i];
-                }
-            }
-            
-            answer += k * m;
-            pointer += m;
+        for (int i = score.length - m; i >= 0; i -= m) {
+            answer += score[i] * m;
         }
         
         return answer;
