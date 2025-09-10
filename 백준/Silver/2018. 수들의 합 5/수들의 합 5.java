62fd6d4f79a813007a82758807ad41b79a25a8dc
Min Namgung <nmin1124@gmail.com>
2025-09-10 12:43:42 +0900

    [Silver V] Title: 수들의 합 5, Time: 216 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 1;
+        int start = 1;
+        int end = 1;
+        int sum = 1;
+        
+        while (end != n) {
+            if (sum == n) {
+                count++;
+                end++;
+                sum += end;
+            } else if (sum > n) {
+                sum -= start;
+                start++;
+            } else if (sum < n) {
+                end++;
+                sum += end;
+            }
+        }
+        
+        System.out.println(count);
+    }
+}
