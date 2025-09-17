6e1b83d12a0ea5e3b1d331a86c4ce13a75ece6a6
Min Namgung <nmin1124@gmail.com>
2025-09-17 14:05:07 +0900

    [Bronze II] Title: 수 정렬하기, Time: 296 ms, Memory: 22392 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) nums[i] = sc.nextInt();
+        
+        /* Bubble Sort */
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    int tmp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = tmp;
+                }
+            }
+        }
+        
+        for (int i = 0; i < n; i++) System.out.println(nums[i]);
+    }
+}
