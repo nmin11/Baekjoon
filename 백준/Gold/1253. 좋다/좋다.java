82da04733672b6c645152674080fd5c0d6989d51
Min Namgung <nmin1124@gmail.com>
2025-09-20 13:35:34 +0900

    [Gold IV] Title: 좋다, Time: 148 ms, Memory: 15020 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -1,98 +1,49 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    static FastReader scan = new FastReader();
-    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    static int N;
-    static int[] A;
+        int n = Integer.parseInt(br.readLine());
+        long[] nums = new long[n];
 
-    static void input() {
-        N = scan.nextInt();
-        A = new int[N + 1];
-        for (int i = 1; i <= N; i++) {
-            A[i] = scan.nextInt();
-        }
-    }
-
-    // target_idx 번째 원소가 서로 다른 두 수의 합으로 표현이 되는가?
-    static boolean func(int target_idx) {
-        int L = 1, R = N;
-        int target = A[target_idx];
-        while (L < R) {
-            if (L == target_idx) L++;
-            else if (R == target_idx) R--;
-            else {
-                if (A[L] + A[R] > target) R--;
-                else if (A[L] + A[R] == target) return true;
-                else L++;
-            }
-        }
-        return false;
-    }
-
-    static void pro() {
-        // 최소, 최대를 빠르게 알기 위한 정렬
-        Arrays.sort(A, 1, N + 1);
-
-        int ans = 0;
-        for (int i = 1; i <= N; i++) {
-            // i 번째 원소가 서로 다른 두 수의 합으로 표현이 되는가?
-            if (func(i)) ans++;
-        }
-        System.out.println(ans);
-    }
-
-    public static void main(String[] args) {
-        input();
-        pro();
-    }
-
-
-    static class FastReader {
-        BufferedReader br;
-        StringTokenizer st;
-
-        public FastReader() {
-            br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            nums[i] = Long.parseLong(st.nextToken());
         }
 
-        public FastReader(String s) throws FileNotFoundException {
-            br = new BufferedReader(new FileReader(new File(s)));
-        }
+        Arrays.sort(nums);
+        int count = 0;
 
-        String next() {
-            while (st == null || !st.hasMoreElements()) {
-                try {
-                    st = new StringTokenizer(br.readLine());
-                } catch (IOException e) {
-                    e.printStackTrace();
+        for (int i = 0; i < n; i++) {
+            long target = nums[i];
+            int left = 0;
+            int right = n - 1;
+
+            while (left < right) {
+                long sum = nums[left] + nums[right];
+
+                if (sum == target) {
+                    if (left == i) {
+                        left++;
+                    } else if (right == i) {
+                        right--;
+                    } else {
+                        count++;
+                        break;
+                    }
+                } else if (sum < target) {
+                    left++;
+                } else {
+                    right--;
                 }
             }
-            return st.nextToken();
         }
 
-        int nextInt() {
-            return Integer.parseInt(next());
-        }
-
-        long nextLong() {
-            return Long.parseLong(next());
-        }
-
-        double nextDouble() {
-            return Double.parseDouble(next());
-        }
-
-        String nextLine() {
-            String str = "";
-            try {
-                str = br.readLine();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return str;
-        }
+        System.out.println(count);
     }
 }
