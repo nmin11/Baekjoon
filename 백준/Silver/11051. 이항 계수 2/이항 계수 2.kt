a6e4ec8cf05d46a6ae1009f742524c93fcbc8d15
Min Namgung <nmin1124@gmail.com>
2026-01-08 13:41:59 +0900

    [Silver II] Title: 이항 계수 2, Time: 124 ms, Memory: 24280 KB -BaekjoonHub

---
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.kt
@@ -0,0 +1,23 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+private val mod = 10_007
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val (n, k) = br.readLine().split(" ").map { it.toInt() }
+    val dp = Array(n + 1) { IntArray(n + 1) }
+
+    for (i in 0..n) {
+        dp[i][0] = 1
+        dp[i][i] = 1
+    }
+
+    for (i in 2..n) {
+        for (j in 1 until i) {
+            dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]) % mod
+        }
+    }
+
+    println(dp[n][k])
+}
