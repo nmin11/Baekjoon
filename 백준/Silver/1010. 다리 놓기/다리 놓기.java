4cab04895e8ccb3bfc4dfd1bfd2cbd9d53a649b6
Min Namgung <nmin1124@gmail.com>
2026-01-10 20:25:50 +0900

    [Silver V] Title: 다리 놓기, Time: 104 ms, Memory: 14268 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        int[][] dp = new int[31][31];
+
+        for (int i = 0; i <= 30; i++) {
+            dp[i][0] = 1;
+            dp[i][i] = 1;
+        }
+
+        for (int i = 2; i <= 30; i++) {
+            for (int j = 1; j < i; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i - 1][j - 1];
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < t; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            sb.append(dp[m][n]).append('\n');
+        }
+
+        System.out.print(sb);
+    }
+}
