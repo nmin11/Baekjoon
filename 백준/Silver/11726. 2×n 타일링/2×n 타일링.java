05ed7570682514a68a7ec618a95a45e3057fe805
Min Namgung <nmin1124@gmail.com>
2026-01-15 14:15:13 +0900

    [Silver III] Title: 2×n 타일링, Time: 104 ms, Memory: 14372 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    private static final int MOD = 10_007;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        if (n >= 1) dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
+        }
+
+        System.out.println(dp[n]);
+    }
+}
