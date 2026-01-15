58725f644b26014020487846de8fed5a3858a5ee
Min Namgung <nmin1124@gmail.com>
2026-01-15 14:17:10 +0900

    [Silver III] Title: 2×n 타일링, Time: 72 ms, Memory: 12224 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.kt
@@ -0,0 +1,18 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+private const val MOD = 10_007
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val n = br.readLine().toInt()
+    val dp = IntArray(n + 1)
+    dp[0] = 1
+    if (n >= 1) dp[1] = 1
+
+    for (i in 2..n) {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % MOD
+    }
+
+    println(dp[n])
+}
