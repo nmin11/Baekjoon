62dae888c0e4e3603bbc1965fb0aef3572e32b7d
Min Namgung <nmin1124@gmail.com>
2026-01-16 15:15:52 +0900

    [Gold V] Title: 1로 만들기 2, Time: 124 ms, Memory: 21988 KB -BaekjoonHub

---
+++ 백준/Gold/12852. 1로 만들기 2/1로 만들기 2.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[n + 1];
+        int[] prev = new int[n + 1];
+        dp[1] = 0;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + 1;
+            prev[i] = i - 1;
+
+            if (i % 2 == 0 && dp[i] > dp[i / 2] + 1) {
+                dp[i] = dp[i / 2] + 1;
+                prev[i] = i / 2;
+            }
+
+            if (i % 3 == 0 && dp[i] > dp[i / 3] + 1) {
+                dp[i] = dp[i / 3] + 1;
+                prev[i] = i / 3;
+            }
+        }
+
+        System.out.println(dp[n]);
+
+        StringBuilder sb = new StringBuilder();
+        int cur = n;
+        while (cur >= 1) {
+            sb.append(cur).append(' ');
+            cur = prev[cur];
+        }
+
+        System.out.println(sb);
+    }
+}
