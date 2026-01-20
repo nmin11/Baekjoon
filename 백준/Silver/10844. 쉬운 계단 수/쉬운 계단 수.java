74854f73559628f2c438a746cae02169aeb1e033
Min Namgung <nmin1124@gmail.com>
2026-01-20 14:12:42 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 104 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    private static final long MOD = 1_000_000_000;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        long[][] dp = new long[n + 1][10];
+
+        for (int i = 1; i <= 9; i++) {
+            dp[1][i] = 1;
+        }
+
+        for (int i = 2; i <= n; i++) {
+            dp[i][0] = dp[i - 1][1];
+            dp[i][9] = dp[i - 1][8];
+
+            for (int j = 1; j <= 8; j++) {
+                dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % MOD;
+            }
+        }
+
+        long answer = 0L;
+        for (int i = 0; i <= 9; i++) {
+            answer = (answer + dp[n][i]) % MOD;
+        }
+
+        System.out.println(answer);
+    }
+}
